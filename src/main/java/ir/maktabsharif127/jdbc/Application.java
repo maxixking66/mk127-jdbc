3a3ef8838faf3b0b2ae936f0cd38dfaad812c913
maxixking66 <maxixking66@gamil.com>
2025-04-10 10:04:01 +0330

    feat: get connection from DriverManager

--- src/main/java/ir/maktabsharif127/jdbc/Application.java
+++ src/main/java/ir/maktabsharif127/jdbc/Application.java
@@ -1,8 +1,24 @@
 package ir.maktabsharif127.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        try (
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:postgresql://localhost:5432/postgres",
+                        "postgres",
+                        "123456789"
+                )
+        ) {
+
+            System.out.println("connection is open");
+
+        }
 
     }
 
