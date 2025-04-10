647ce0aeeebbd974c26636eea2a9e98ad7adfc2f
maxixking66 <maxixking66@gamil.com>
2025-04-10 10:38:26 +0330

    refactor: use statement for executing query

--- src/main/java/ir/maktabsharif127/jdbc/Application.java
+++ src/main/java/ir/maktabsharif127/jdbc/Application.java
@@ -1,8 +1,6 @@
 package ir.maktabsharif127.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Application {
 
@@ -18,10 +16,23 @@ public class Application {
 
             System.out.println("connection is open");
 
+            try (Statement statement = connection.createStatement()) {
+                try (ResultSet resultSet = statement.executeQuery("select id, first_name, last_name, age from users limit 10")) {
+                    int row = 1;
+                    while (resultSet.next()) {
+                        int id = resultSet.getInt(1);
+//                        int id = resultSet.getInt("id");
+                        String firstName = resultSet.getString(2);
+                        String lastName = resultSet.getString(3);
+                        int age = resultSet.getInt(4);
+
+                        System.out.println(row++ + ")" + id + " " + firstName + " " + lastName + " " + age);
+
+                    }
+                }
+            }
         }
 
     }
 
 }
-
-
